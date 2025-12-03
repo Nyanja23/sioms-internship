a936bc0c1423cf770807eed09a313ed98ce4cedf
Nyanja23 <josephmendes256@gmail.com>
2025-12-03 13:09:30 +0300

    Updated Stuff for Production

--- sioms_backend/settings.py
+++ sioms_backend/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
@@ -77,11 +79,19 @@ WSGI_APPLICATION = 'sioms_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
